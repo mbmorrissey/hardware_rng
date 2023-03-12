492730447174383ebbea5089fbcef096252ffdfe
mbmorrissey <michael.morrissey@st-andrews.ac.uk>
2023-03-12 17:53:24 +0000

    add uniform and poisson R scripts

---
+++ r_scripts/geiger_rng_poisson.R
@@ -0,0 +1,60 @@
+// geiger_rng_poisson.R
+// Michael Morrissey
+// 12 March 2023
+// part of a light-hearted project to use a DIY Geiger counter kit to
+// make a hardware random number generator
+// https://github.com/mbmorrissey/hardware_rng
+
+library(serial)
+
+// see what ports are available
+listPorts()
+
+// will need to figure out from listPorts() what port
+// the arduino is on
+con <- serialConnection(name = "geiger",port = "cu.usbmodem1101"
+,mode = "9600,n,8,2"
+,newline = 1 )
+
+open(con)
+
+// number of random numbers to collect
+n <- 1000
+counts<-array(dim=maxCounts)
+reads <- 0
+
+// discard anything already in the incoming serial buffer
+a<-read.serialConnection(con)
+a<-""
+
+while(reads<n){
+
+  // wait for data to come in
+  while(a==""){
+    Sys.sleep(0.1) // seems overkill to monitor for input constantly
+    a<-read.serialConnection(con)
+  }
+  a<-as.numeric(a)
+  print(a)
+  reads<-reads+1
+  counts[reads]<-a
+  a<-""
+
+  // live plot as data accumulate
+  m<-max(counts,na.rm=TRUE)
+  h<-hist(counts, breaks=seq(-0.5,m+0.5,by=1),main="",xaxt='n')
+  axis(side=1,0:m)
+  text(m*0.78,max(h$counts),paste("n: ",reads,sep=""),adj=0)
+  text(m*0.78,max(h$counts)*0.9,paste("mean: ",round(
+                    mean(counts,na.rm=TRUE),2),sep=""),adj=0)
+  text(m*0.78,max(h$counts)*0.8,paste("variance: ",round(
+                    var(counts,na.rm=TRUE),2),sep=""),adj=0)
+}
+
+// generate final plot of example data
+pdf("./poisson_rng_example.pdf",height=5,width=5
+m<-max(counts,na.rm=TRUE)
+hist(counts, breaks=seq(-0.5,m+0.5,by=1),main="",xaxt='n')
+axis(side=1,0:m)
+dev.off()
+
---
+++ r_scripts/geiger_rng_uniform.R
@@ -0,0 +1,57 @@
+// geiger_rng_uniform.R
+// Michael Morrissey
+// 12 March 2023
+// part of a light-hearted project to use a DIY Geiger counter kit to
+// make a hardware random number generator
+// https://github.com/mbmorrissey/hardware_rng
+
+library(serial)
+
+// see what ports are available
+listPorts()
+
+// will need to figure out from listPorts() what port
+// the arduino is on
+con <- serialConnection(name = "geiger",port = "cu.usbmodem101"
+,mode = "9600,n,8,2"
+,newline = 1 )
+
+open(con)
+
+// number of random numbers to collect
+n<-1000
+vals<-array(dim=n)
+reads <- 0
+
+// inital read to clear input in case multiple data have 
+// already been sent
+a<-read.serialConnection(con)
+a<-""
+
+// discard first datum (if the first once the hardware is powered
+// it will be NA) 
+while(a=="") a<-read.serialConnection(con)
+
+// collect n uniform random numbers
+while(reads<n){
+  while(a==""){
+    Sys.sleep(0.1) // seems overkill to monitor for input constantly
+    a<-read.serialConnection(con)
+  }
+  a<-as.numeric(a)
+  print(a)
+  reads<-reads+1
+  vals[reads]<-a
+  a<-""
+
+  // add data to live plot
+  h<-hist(vals, breaks=seq(0,1,by=0.05),main="",xaxt='n')
+  axis(side=1,seq(0,1,by=0.05))
+  text(0.78,max(h$counts)*1.05,xpd=TRUE,paste("n: ",reads,sep=""),adj=0)
+}
+
+// generate final plot of example data
+pdf("./uniform_rng_example.pdf",height=5,width=5
+hist(vals, breaks=seq(0,1,by=0.05),main="",xaxt='n')
+axis(side=1,seq(0,1,by=0.05))
+dev.off()
